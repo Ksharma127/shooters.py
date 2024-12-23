4b042f578ad83fad94459f1ebe3e2856c8bbca92
Ksharma127 <kris.sharma.2009@gmail.com>
2024-12-23 19:58:04 +0000

    Add files via upload

---
+++ shoot.py
@@ -0,0 +1,21 @@
+import pgzrun
+
+WIDTH = 500
+HEIGHt = 500
+
+Title="good shot game"
+
+#any picture that we need to make as a charachter - we useActor function
+image=Actor("alien") #this is the path to the image  
+
+def draw():
+    screen.clear()#this clears the screen
+    screen.fill(color=(235, 143, 52))
+
+
+
+
+
+
+
+pgzrun.go()
